b37c37d98cbc7dfd41ab48c8cdcce593f67a52ad
Martin <72672470+Martin-Aziz@users.noreply.github.com>
2026-03-08 09:58:06 +0100

    Initial Commit

---
+++ Moonlit Reel/Moonlit_ReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonlit_ReelApp.swift
+//  Moonlit Reel
+//
+//  Created by Martin Aziz on 08.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Moonlit_ReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
